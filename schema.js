a03070d18bb84c7d4f3e250c990ee0c99a85a176
seun jay <seunjay92@gmail.com>
2019-07-25 12:36:11 +0100

    specify  root query and customer type

--- schema.js
+++ schema.js
@@ -6,4 +6,44 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
-module.exports = new GraphQLSchema({});
+//Hardcoded data
+const customers = [
+  { id: "1", name: "John Doe", email: "john@gmail.com", age: 35 },
+  { id: "2", name: "Steve Smith", email: "smith@gmail.com", age: 25 },
+  { id: "3", name: "Sarah Williams", email: "sara@gmail.com", age: 32 }
+];
+
+//Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
