9bb19e7e86ff0d19202299a46705f7c9f6ed448b
seun jay <seunjay92@gmail.com>
2019-07-25 13:38:53 +0100

    require axios to make http requests

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,12 +8,14 @@ const {
   GraphQLNonNull
 } = require("graphql");
 
+/*
 //Hardcoded data
 const customers = [
   { id: "1", name: "John Doe", email: "john@gmail.com", age: 35 },
   { id: "2", name: "Steve Smith", email: "smith@gmail.com", age: 25 },
   { id: "3", name: "Sarah Williams", email: "sara@gmail.com", age: 32 }
 ];
+*/
 
 //Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -36,18 +39,27 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
+        /*
         for (let i = 0; customers.length; i++) {
           if (customers[i].id === args.id) {
             return customers[i];
           }
         }
+        */
+        return axios
+          .get("http://localhost:3000/customers/" + args.id)
+          .then(res => res.data);
       }
     },
     //all customers
-    customers:{
+    customers: {
       type: new GraphQLList(CustomerType),
-      resolve(parentValue, args){
-        return customers
+      resolve(parentValue, args) {
+        //return customers;
+
+        return axios
+          .get("http://localhost:3000/customers/")
+          .then(res => res.data);
       }
     }
   }
