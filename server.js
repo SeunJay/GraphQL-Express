f39425e92e4af339bf2f5a12e219b84f359a3bf3
seun jay <seunjay92@gmail.com>
2019-07-25 11:44:23 +0100

    require express-graphql and create graphql endpoint for client to interact with it

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
